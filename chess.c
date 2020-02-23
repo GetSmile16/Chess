197a2c4ec1f283f9b49d87e483d84e8d98c9f14b
Vladislav <ivlad160101@gmail.com>
2020-02-23 19:40:52 +0700

    Fix chess.c

--- chess.c
+++ chess.c
@@ -14,7 +14,7 @@ int main()
                "pppppppp",
                "rheqkehr"};
     for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE)
+        for (int j = 0; j < SIZE; j++)
             ; j++)
         {
             printf("%c", chessboard[i][j]);
